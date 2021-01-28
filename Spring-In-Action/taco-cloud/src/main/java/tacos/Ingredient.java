8557d33a92cd83e83e7d11419c3d312d5a77d30d
jong yoon <dhorlawhddbs@naver.com>
2021-01-28 21:08:01 +0900

    add : Spring in Action 2장 코드 완료

---
+++ Spring-In-Action/taco-cloud/src/main/java/tacos/Ingredient.java
@@ -0,0 +1,14 @@
+package tacos;
+
+import lombok.Data;
+
+@Data
+public class Ingredient {
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
